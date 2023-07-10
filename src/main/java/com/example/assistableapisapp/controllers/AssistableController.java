922c9f67db7fcc7362e217d660706629e59bc54a
DavidMumpasa <davidebula642@gmail.com>
2023-07-10 21:52:06 +0200

    Account Verification api

---
+++ src/main/java/com/example/assistableapisapp/controllers/AssistableController.java
@@ -0,0 +1,35 @@
+package com.example.demo1.controller;
+
+import com.example.demo1.bussinessLogic.AccountService;
+import okhttp3.*;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+
+@RestController
+public class AssistableController {
+    private final AccountService accountService;
+    public AssistableController( AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping("//bank-account-verification")
+    public String  verifyBankAccount(@RequestParam("accountNumber") String accountNumber,@RequestParam("idNumber") String idNumber
+            ,@RequestParam("accountType") String accountType,@RequestParam("branchCode") String branchCode,@RequestParam("branchCode") String yourReference) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        // Build the request body
+        RequestBody body = accountService.buildRequestBody(accountNumber,idNumber,accountType,branchCode,yourReference);
+        // Build Request
+        Request request = accountService.buildRequest(body);
+
+        Response response = client.newCall(request).execute();
+        String responseBody = response.body().string();
+
+        return responseBody;
+    }
+}
---
+++ src/main/java/com/example/assistableapisapp/services/AccountService.java
@@ -0,0 +1,49 @@
+package com.example.demo1.bussinessLogic;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import org.springframework.stereotype.Service;
+
+@Service
+@Setter
+@Getter
+public class AccountService {
+    public String errorMessage;
+    public String url;
+
+
+    public RequestBody buildRequestBody(String accountNumber,String idNumber,String accountType, String branchCode,String yourReference){
+
+        okhttp3.MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = new MultipartBody.Builder().setType(MultipartBody.FORM)
+                .addFormDataPart("memberkey", "PB-FLU001")
+                .addFormDataPart("password","062fd17a5e1b41b4e627406410e4769d")
+                .addFormDataPart("bvs_details[accountNumber]",accountNumber)
+                .addFormDataPart("bvs_details[idNumber]",idNumber)
+                .addFormDataPart("bvs_details[accountType]",accountType)
+                .addFormDataPart("bvs_details[branchCode]",branchCode)
+                .addFormDataPart("bvs_details[yourReference] ",yourReference)
+                .build();
+
+        return body;
+    }
+
+    public Request buildRequest(RequestBody body){
+        // Build the request
+        Request request = new Request.Builder()
+                .url("https://veriid.com/PBVerify/webservice/pbverify-bank-account-verification-v3")
+                .method("POST", body)
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Cookie", "laravel_session=eyJpdiI6IlQ0N0U2NENXUk1QMTV1SjlpUkM4dmc9PSIsInZhbHVlIjoiZVZHRFJSelIwbzE4b0tGV2F0ZnZFcFlRbmRyeFJEU3hEMTBLMWN4Zyttd1NqODRVaWQycjk1cFlLNThxejFYRk1nXC9Ta2VEZzU1b3hrN3pkQVBqXC8yQT09IiwibWFjIjoiMTA5MDcwYTY2Yzk1NzMyNmE4OTU2Y2M4YjlkODBlZmM4MWI2MjQzZWI5MjExZDk1OWM2YTQ2OTBkOGRmZDFjMSJ9; pbverify-chocchip=3647aed5ed80c78a7c47c5dbcd3d6516|1676eed65626ff656c4990ede0fde4aa")
+                .build();
+
+        return request;
+    }
+
+
+}
