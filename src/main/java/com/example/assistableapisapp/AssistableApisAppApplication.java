0921ae4afa97a944b64b7a9cd912eb5693405504
DavidMumpasa <davidebula642@gmail.com>
2023-07-10 15:14:59 +0200

    first commit

---
+++ src/main/java/com/example/assistableapisapp/AssistableApisAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.assistableapisapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistableApisAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssistableApisAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/assistableapisapp/AssistableApisAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assistableapisapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistableApisAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
